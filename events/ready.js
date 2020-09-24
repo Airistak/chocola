6586fff1291e1dc2945a2ba44ca96e562e3d8e0f
kurisuchanch <64631783+kurisuchanch@users.noreply.github.com>
2020-09-24 23:24:20 +0700

    Update ready.js
    kontlo

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 module.exports = client => {
   console.log("The bot is ready!")
   function randStatus() {
-        let status = [`Odading Mang Oleh`,`Rasanya Anjim Banget`]
+        let status = [`Ahoy Ahoy`,`Senchou Marine Desu`,`Houshou Marine`]
         let rstatus = Math.floor(Math.random() * status.length);
         client.user.setActivity(status[rstatus], {type: "STREAMING", url:"https://twitch.tv/kurisu_ch"});
     }; setInterval(randStatus, 1000)
